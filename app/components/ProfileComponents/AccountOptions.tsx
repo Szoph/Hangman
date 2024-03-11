b7b9d0e828451f3ff50214d2a5e302fc0f687ae7
Tony K. <tony-koke@outlook.com>
2024-03-11 15:58:18 +0000

    H-3 Profile Page Link Update

--- app/components/ProfileComponents/AccountOptions.tsx
+++ app/components/ProfileComponents/AccountOptions.tsx
@@ -1,13 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const AccountOptions = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    console.log("Log out user");
+    router.push("/")
+  }
+
+  const handleDeleteAccount = () => {
+    console.log("Log out user");
+    router.push("/")
+  }
+
   return (
     <div className="account-section">
       {/* Logout Account */}
-      <div className="logout-button flex-center account-buttons">
+      <div onClick={handleLogout} className="logout-button flex-center account-buttons">
         <p>Logout</p>
       </div>
 
       {/* Delete Account */}
-      <div className="delete-button flex-center account-buttons">
+      <div onClick={handleDeleteAccount} className="delete-button flex-center account-buttons">
         <p>Delete Account</p>
       </div>
     </div>
