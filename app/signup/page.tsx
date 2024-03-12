f58fab1379585be558e3834ec6daddd7e37b9d54
Sophie Woodcock <szoph101@hotmail.co.uk>
2024-03-12 22:43:41 +0000

    added sign up page and styling

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,11 @@
+"use client" 
+import SignUpComponent from "../components/SignUpContainer/SignUpComponent";
+import "@/app/styles/SignUpStyles/signup.css";
 const SignUp = () => {
     return (
-        <></>
+        <>
+        <SignUpComponent />
+        </>
     )
 };
 
