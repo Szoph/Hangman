1ff3ca4be07fdcfe7fbe9fa9e260a2091d5004ae
Tony K. <tony-koke@outlook.com>
2024-03-13 12:24:48 +0000

    H-12 Added No Genre Text

--- app/components/GenreMenuComponents/SearchGenre.tsx
+++ app/components/GenreMenuComponents/SearchGenre.tsx
@@ -30,6 +30,13 @@ const SearchGenre = () => {
 
   const [searchValue, setSearchValue] = useState<SearchState>({});
 
+  // Filter the genres based on the search input
+  const filteredGenres = searchValue["search"]
+    ? tempGenre.filter((genre) =>
+        genre.toLowerCase().includes(searchValue["search"]?.toLowerCase())
+      )
+    : tempGenre;
+
   return (
     <div className="search-genre-container">
       <form className="search-bar-container">
@@ -44,29 +51,20 @@ const SearchGenre = () => {
       </form>
 
       <div className="other-genre-container">
-        {searchValue["search"]
-          ? tempGenre
-              .filter((genre) =>
-                genre
-                  .toLowerCase()
-                  .includes(searchValue["search"]?.toLowerCase())
-              )
-              .map((filteredGenre, index) => (
-                <GenreCards
-                  key={index}
-                  height="14em"
-                  width="18em"
-                  genre={filteredGenre}
-                />
-              ))
-          : tempGenre.map((genre, index) => (
-              <GenreCards
-                key={index}
-                height="14em"
-                width="18em"
-                genre={genre}
-              />
-            ))}
+        {filteredGenres.length > 0 ? (
+          filteredGenres.map((filteredGenre, index) => (
+            <GenreCards
+              key={index}
+              height="14em"
+              width="18em"
+              genre={filteredGenre}
+            />
+          ))
+        ) : (
+          <div className="no-genre">
+            <p style={{ fontSize: "40px"}}>Genre doesn't exist!</p>
+          </div>
+        )}
       </div>
     </div>
   );
