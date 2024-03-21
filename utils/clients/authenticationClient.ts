03898f4c2b0bf67b5723176b83203a44f7362183
Tony K. <tony-koke@outlook.com>
2024-03-21 02:28:22 +0000

    H-17 Sign up UI Update

--- utils/clients/authenticationClient.ts
+++ utils/clients/authenticationClient.ts
@@ -1,4 +1,30 @@
+import axios from "axios";
+
+const SERVER_URL: string = "http://127.0.0.1:5000/auth";
+
+interface AuthResponse {
+    success: boolean;
+    message?: any;
+    error?: string;
+}
+
 class AuthClient {
+    async userExists(username: string): Promise<AuthResponse> {
+        const userExisetence: any = await axios.get(`${SERVER_URL}/user_exists/${username}`);
+
+        if (!userExisetence.data.success) {
+            return {
+                success: false,
+                message: "User doesn't exist!",
+            }
+        }
+
+        return {
+            success: true,
+            message: "User already exists!",
+        };
+    }
+
     getUser(): void {
 
     }
@@ -7,8 +33,24 @@ class AuthClient {
 
     }
 
-    signUpUser(): void {
+    async signUpUser(username: string, password: string): Promise<AuthResponse> {
+        const params = {
+            username,
+            password
+        }
+        const signUp = await axios.post(`${SERVER_URL}/signup`, params);
 
+        if (signUp.data.success) {
+            return {
+                success: true,
+                message: signUp.data.data
+            }
+        }
+
+        return {
+            success: false,
+            message: signUp.data.error
+        }
     }
 
     deleteUser(): void {
