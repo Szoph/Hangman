ae704d69ffd53ae20478b0f9c40187191fdb6ee2
Tony K. <tony-koke@outlook.com>
2024-03-25 10:37:42 +0000

    Store update

--- redux/provider.tsx
+++ redux/provider.tsx
@@ -4,7 +4,7 @@ import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 
-export function HangmanReduxProvider({
+export function UserReduxProvider({
   children,
 }: {
   children: React.ReactNode;
